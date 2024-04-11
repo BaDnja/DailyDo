5b5dc73b5f60c876d938b85a47be09568088fffc
Amar Badnjevic <badnja25@gmail.com>
2024-04-11 11:39:09 +0200

    initial list items

---
+++ src/app/tasks/tasks.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import {TaskListComponent} from "./task-list/task-list.component";
+
+@Component({
+  selector: 'app-tasks',
+  standalone: true,
+  imports: [
+    TaskListComponent
+  ],
+  templateUrl: './tasks.component.html',
+  styleUrl: './tasks.component.css'
+})
+export class TasksComponent {
+
+}
