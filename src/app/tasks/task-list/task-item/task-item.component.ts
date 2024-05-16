f005ad669c98c070546ff09341f86243ced3e853
Amar Badnjevic <badnja25@gmail.com>
2024-05-16 11:46:35 +0200

    add checkbox change functionality

--- src/app/tasks/task-list/task-item/task-item.component.ts
+++ src/app/tasks/task-list/task-item/task-item.component.ts
@@ -1,18 +1,31 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {FormControl, ReactiveFormsModule} from '@angular/forms';
 import {Task} from "../../task.model";
 import {TasksService} from "../../tasks.service";
 
 @Component({
   selector: 'app-task-item',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule
+  ],
   templateUrl: './task-item.component.html',
   styleUrl: './task-item.component.css'
 })
-export class TaskItemComponent {
+export class TaskItemComponent implements OnInit {
   @Input() task!: Task;
+  taskCheckbox: FormControl = new FormControl();
 
   constructor(private tasksService: TasksService) {
+  }
+
+  ngOnInit() {
+    this.taskCheckbox.valueChanges.subscribe(value => {
+      const task = this.tasksService.getTasks().find(task => task == this.task);
+      if (task) {
+        task.isDone = value;
+      }
+    })
   }
 
   onSelected() {
