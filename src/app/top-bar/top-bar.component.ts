2537a51199a525cc58b31d26e22473e5d09910f1
Amar Badnjevic <badnja25@gmail.com>
2024-05-23 13:27:28 +0200

    show confirmation dialog on attempt to clear data

--- src/app/top-bar/top-bar.component.ts
+++ src/app/top-bar/top-bar.component.ts
@@ -1,19 +1,35 @@
 import {Component} from '@angular/core';
 import {RouterLink} from "@angular/router";
 import {TasksService} from "../tasks/tasks.service";
+import {ConfirmationDialogComponent} from "../confirmation-dialog/confirmation-dialog.component";
+import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-top-bar',
   standalone: true,
   imports: [
-    RouterLink
+    RouterLink,
+    ConfirmationDialogComponent,
+    NgIf
   ],
   templateUrl: './top-bar.component.html',
   styleUrl: './top-bar.component.css'
 })
 export class TopBarComponent {
+  showConfirmation: boolean = false;
 
   constructor(private tasksService: TasksService) {
+  }
+
+  onClearData() {
+    this.showConfirmation = true;
+  }
+
+  onConfirmDeletion(confirm: boolean) {
+    this.showConfirmation = false
+    if (confirm) {
+      this.clearData();
+    }
   }
 
   clearData() {
