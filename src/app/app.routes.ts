4eba7637eb5e92f28527272b493fa800926e6464
Amar Badnjevic <badnja25@gmail.com>
2024-05-25 20:47:09 +0200

    groups component and routing to groups

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,12 @@ import {TaskDetailComponent} from "./tasks/task-detail/task-detail.component";
 import {TaskListComponent} from "./tasks/task-list/task-list.component";
 import {ListsComponent} from "./lists/lists.component";
 import {ListDetailComponent} from "./lists/list-detail/list-detail.component";
+import {GroupsComponent} from "./groups/groups.component";
 
 export const routes: Routes = [
   {path: '', component: TaskListComponent},
   {path: 'tasks/:id', component: TaskDetailComponent},
   {path: 'lists', component: ListsComponent},
   {path: 'lists/:id', component: ListDetailComponent},
-
+  {path: 'groups', component: GroupsComponent},
 ];
